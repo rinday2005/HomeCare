21245e01f10649c5a6b1fef48b31c29096d9b6ce
quanmaidang-tech <quanmaidang@gmail.com>
2026-01-30 14:35:48 +0700

    Delete HomeCare-main directory

--- HomeCare-main/FE/src/components/ui/loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-    return (
-        <div className="flex items-center justify-center min-h-[50vh] w-full">
-            <div className="loader"></div>
-        </div>
-    );
-};
-
-export default Loader;
